2015fcfafe54f5dd0e65a74b4a26d903f1a8f1d4
lfnandoo <devfernandoof@gmail.com>
2020-10-11 23:34:33 -0300

    register and unregister

--- src/components/Model/ModelSection/styles.ts
+++ src/components/Model/ModelSection/styles.ts
@@ -3,5 +3,4 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100vh;
   scroll-snap-align: start;
-
 `;
--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -1,9 +1,48 @@
 import React from "react";
 
+import ModelsContext, { CarModel } from "../ModelsContext";
+
 import { Container } from "./styles";
 
 const ModelsWrapper: React.FC = ({ children }) => {
-  return <Container>{children}</Container>;
+  const wrapperRef = React.useRef<HTMLDivElement>(null);
+
+  const [registeredModels, setRegisteredModels] = React.useState<CarModel[]>(
+    []
+  );
+
+  const registerModel = React.useCallback((model: CarModel) => {
+    setRegisteredModels((state) => [...state, model]);
+  }, []);
+
+  const unregisterModel = React.useCallback((modelName: string) => {
+    setRegisteredModels((state) =>
+      state.filter((model) => model.modelName !== modelName)
+    );
+  }, []);
+
+  const getModelByName = React.useCallback(
+    (modelName: string) => {
+      return (
+        registeredModels.find((item) => item.modelName === modelName) || null
+      );
+    },
+    [registeredModels]
+  );
+
+  return (
+    <ModelsContext.Provider
+      value={{
+        wrapperRef,
+        registeredModels,
+        registerModel,
+        unregisterModel,
+        getModelByName
+      }}
+    >
+      <Container ref={wrapperRef}>{children}</Container>
+    </ModelsContext.Provider>
+  );
 };
 
 export default ModelsWrapper;
